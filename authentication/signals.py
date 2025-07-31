f86485fa3bf1313f7a870873a00973878e2a1deb
Misbah yousaf <107263622+Misbahkk@users.noreply.github.com>
2025-07-31 21:29:18 +0500

    Update signal

--- authentication/signals.py
+++ authentication/signals.py
@@ -3,21 +3,28 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from courses.models import Teacher
+from courses.models import Teacher, Studentprofile 
 
 @receiver(post_save, sender=User)
-def create_teacher_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     """
-    Create Teacher profile when user with role 'teacher' is created
+    Automatically create profile for Teacher or Student when a new user is created
     """
-    if created and instance.role == 'teacher':
-        Teacher.objects.create(user=instance)
+    if created:
+        if instance.role == 'teacher':
+            Teacher.objects.create(user=instance)
+        elif instance.role == 'student':
+            StudentProfile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
-def save_teacher_profile(sender, instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs):
     """
-    Save Teacher profile when user is saved
+    Save the profile when user is saved (if exists)
     """
     if instance.role == 'teacher':
         teacher, created = Teacher.objects.get_or_create(user=instance)
         teacher.save()
+    elif instance.role == 'student':
+        student, created = StudentProfile.objects.get_or_create(user=instance)
+        student.save()
